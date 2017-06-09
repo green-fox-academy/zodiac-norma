31e14ab970b91c8637740e59e131f5c79d5ca652
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-06-09 16:34:58 +0200

    first app created

--- booking-frontend-ng/src/app/app.component.ts
+++ booking-frontend-ng/src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  ngOnInit() {
+  }
+  constructor() {
+  }
+
+  items = ["amazing", "awesome", "blithesome", "excellent", "fabulous", "fantastic", "favorable", "fortuitous", "great", "incredible", "ineffable", "mirthful", "outstanding", "perfect", "propitious", "remarkable", "smart", "spectacular", "splendid", "stellar", "stupendous", "super", "ultimate", "unbelievable", "wondrous"];
+
+  itemsFiltered = this.items;
+  username: string;
+
+  onChange = function(username) {
+    this.itemsFiltered = [];
+    //for (let i = 0; i < this.items.length; i++) {
+     // if (this.items[i].includes(username)) {
+     //   this.itemsFiltered.push(this.items[i]);
+      //}
+    //}
+
+    this.itemsFiltered = this.items.filter(item => {
+      return item.indexOf(username) != -1;
+    });
+  }
 }
