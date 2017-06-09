f0ec30d5a281b509280be38424a08eb118672c63
Pamela Paprasz <papraszpamela@gmail.com>
2017-06-09 16:46:54 +0200

    static file serving updated

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 
 var port = process.env.PORT || 8080
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/bookiing-frontend-ng/dist'));
 
 app.get('/', function(req, res){
     res.sendfile(__dirname + '/booking-frontend-ng/dist/index.html');
