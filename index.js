4ec34a5f082dcf75b0c45e8426db561c36cafe94
Pamela Paprasz <papraszpamela@gmail.com>
2017-06-09 16:43:26 +0200

    (refact): index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var port = process.env.PORT || 8080
 app.use(express.static(__dirname + '/dist'));
 
 app.get('/', function(req, res){
-    res.sendfile(__dirname + '/first-heroku-app/dist/index.html');
+    res.sendfile(__dirname + '/booking-frontend-ng/dist/index.html');
 })
 
 app.listen(port, function(){
