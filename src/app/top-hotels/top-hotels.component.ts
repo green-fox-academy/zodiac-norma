3197d7527e7a9a39bd1b7df9250c5b3f649e2d30
Pamela Paprasz <papraszpamela@gmail.com>
2017-06-14 16:41:51 +0200

    data from server is rendered

--- src/app/top-hotels/top-hotels.component.ts
+++ src/app/top-hotels/top-hotels.component.ts
@@ -1,35 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
-
-export class Hotel {
-  id: number;
-  name: string;
-  descr: string;
-}
  
 
 @Component({
   selector: 'app-top-hotels',
   templateUrl: './top-hotels.component.html',
-  styleUrls: ['./top-hotels.component.scss']
+  styleUrls: ['./top-hotels.component.scss'],
+  providers: [TopHotelsComponent]
 })
 export class TopHotelsComponent implements OnInit {
+
+  hotels: [null];
 
   constructor(private http: Http) { }
 
   fetchData(){
     return this.http.get('https://bookingnorma.glitch.me/toprooms').map(
-    (response) => response.json
+    (response => response.json())
     ).subscribe(
-    (data) => console.log(data)
-    
+    (hotels => {
+      this.hotels = hotels
+    }) 
     )
   }
 
   ngOnInit() {
+    this.fetchData()
   }
-
 }
 
 
