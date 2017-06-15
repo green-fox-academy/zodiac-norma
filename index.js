68fdece17876008220028c2998660fe923011561
Daniel Korosi <daniel.korosi42@gmail.com>
2017-06-15 16:01:54 +0200

    slider: shifting array function works

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var app = express();
 var port = process.env.PORT || 8080 || 9876
 
 app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/src/app/slider/pics'));
 
 app.get('/', function(req, res){
     res.sendfile(__dirname + '/dist/index.html');
--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -6,10 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./slider.component.scss']
 })
 export class SliderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  
+  constructor() { 
+    this.createObject()
   }
 
+   ngOnInit() {
+  }
+
+  rawData = [{src: 'img1.jpg'}, {src: 'img2.jpg'}, {src: 'img3.jpg'}, {src: 'img4.jpg'}, {src: 'img5.jpg'}];
+  imageData = [];
+  
+  createObject() {
+    for (let i = 0; i < this.rawData.length; i++) {
+      this.imageData.push({file: this.rawData[i].src, class: 'imgItem'})
+    }
+    /*this.imageData[1].class = 'translated'*/
+    return this.imageData
+  }
+
+
+
+  shiftArray(className, original) {
+    this.rawData.splice(this.rawData.length, 0, this.rawData[0])
+    this.rawData.shift()
+    this.imageData = []
+    this.createObject()
+    this.imageData[1].class += " " + className;
+    //this.imageData[1].class = original;
+    //console.log(this.rawData);
+    console.log(this.imageData);
+
+  }
+  
 }
