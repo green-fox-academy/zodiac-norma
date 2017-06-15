e48a8cd50c28e0b06b3a7b621bfcb4d6742c78c6
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-06-15 09:32:50 +0200

    branch merge prepared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { SliderComponent } from './slider/slider.component';
@@ -20,9 +21,10 @@ import { FooterComponent } from './footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [TopHotelsComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
