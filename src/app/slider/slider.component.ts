319b59817efdd94b2f4f1d10b44bf2877790e613
Daniel Korosi <daniel.korosi42@gmail.com>
2017-06-16 12:31:22 +0200

    slider works back and forth, switches back to the start when reached the end of the array when pressing forward, images changed to divs

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -5,52 +5,66 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.scss']
 })
+
 export class SliderComponent implements OnInit {
   
   constructor() { 
     this.createObject()
   }
-
    ngOnInit() {
   }
 
   //rawData = [{src: 'http://lorempixel.com/400/200/sports/'}, {src: 'http://lorempixel.com/400/205/sports/'}, {src: 'http://lorempixel.com/400/210/sports/'}, {src: 'http://lorempixel.com/400/215/sports/'}, {src: 'http://lorempixel.com/400/220/sports/'}];
-  rawData = [{src: 'img1.jpg'}, {src: 'img2.jpg'}, {src: 'img3.jpg'}, {src: 'img4.jpg'}, {src: 'img5.jpg'}];
+  rawData = [{src: 'img1.jpg'}, {src: 'img2.jpg'}, {src: 'img3.jpg'}, {src: 'img4.jpg'}, {src: 'img5.jpg'}, {src: 'img6.jpg'}, {src: 'img7.jpg'}];
   
   imageData = [];
-  //dataDisplayed = [];
-  classIndex = 0
+  classIndex = 0;
+
   createObject() {
     for (let i = 0; i < this.rawData.length; i++) {
       this.imageData.push({file: this.rawData[i].src, class: 'img-item'})
     }
-   //this.imageData[this.classIndex].class = className;
-    //this.dataDisplayed = this.imageData.reverse()
     this.imageData[0].class = 'actual-front';
     return this.imageData
   }
 
   setClassIndex(x, className) {
-    if (this.classIndex >= 0) {
+    if (this.classIndex >= 0 && this.classIndex < this.rawData.length-1) {
       this.classIndex += x;
-    } else if (this.classIndex >= this.rawData.length-1) {
+      this.imageData[this.imageData.length-1].class = 'img-item';
+    } else if (this.classIndex === this.rawData.length-1) {
         this.classIndex = 0;
-        
-    } else if (this.classIndex < 0){
+    } else if (this.classIndex === 0){
         this.classIndex = this.rawData.length-1
     }
-    
-    this.changeClasses(className, x)
+    if (x === 1) {
+      this.setClassesForward(className, x)
+    } else if (x === -1) {
+      this.setClassesBackward(className, x)
+    }
   }
 
-  changeClasses(classInput, x) {
+  setClassesForward(classInput, x) {
     this.imageData[this.classIndex].class = classInput;
+    if (this.classIndex === 0) {
+      this.imageData[this.imageData.length-1].class = 'actual-front';
+      this.imageData[this.imageData.length-2].class = 'img-item';
+    } else {
+      this.imageData[this.classIndex-x].class = 'actual-front';
+      this.imageData[this.classIndex-(2*x)].class = 'img-item';
+    }
+  }
+
+  setClassesBackward(classInput, x) {
+    this.imageData[this.classIndex].class = classInput;
+    if (this.classIndex === this.imageData.length-1) {
+      this.imageData
+    }
     this.imageData[this.classIndex-x].class = 'actual-front';
     this.imageData[this.classIndex-(2*x)].class = 'img-item';
     //if (this.imageData)
-   
+  
   }
-
   
   
 
