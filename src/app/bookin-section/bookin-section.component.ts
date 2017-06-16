0e7b61e3e42688d7c624854b0cd988fe4b0ff526
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-06-16 08:44:26 +0200

    no options in datepicker try

--- src/app/bookin-section/bookin-section.component.ts
+++ src/app/bookin-section/bookin-section.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {IMyDpOptions} from 'mydatepicker';
+import { IMyDpOptions } from 'mydatepicker';
 
 @Component({
   selector: 'app-bookin-section',
