cc8987323a587e4f47ef72e2c8a650ae669786fa
Daniel Korosi <daniel.korosi42@gmail.com>
2017-06-18 19:32:53 +0200

    slider refactored

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -28,7 +28,7 @@ export class SliderComponent implements OnInit {
     return this.imageData
   }
 
-  setClassIndex(x, className) {
+  /*setClassIndex(x, className) {
     this.classIndex += x;
     if (this.classIndex > 0 && this.classIndex < this.imageData.length) {
       this.imageData[this.imageData.length-1].class = 'img-item';
@@ -67,5 +67,42 @@ export class SliderComponent implements OnInit {
       this.imageData[this.classIndex-x].class = 'actual-front';
       this.imageData[this.classIndex-(2*x)].class = 'img-item';
     }
+  }
+}*/
+    
+  setClassIndex(x, className) {
+    this.classIndex += x;
+    if (this.classIndex > 0 && this.classIndex < this.imageData.length) {
+      this.imageData[this.imageData.length-1].class = 'img-item';
+      this.imageData[0].class = 'img-item';
+    } else if (this.classIndex === this.imageData.length) {
+        this.classIndex = 0;
+    } else if (this.classIndex === -1){
+        this.classIndex = this.imageData.length-1;
+    }
+    this.setClassNames(className, x)
+  }
+
+  setClassNames(className, x) {
+    this.imageData[this.classIndex].class = className;
+
+    if (x === 1 && this.classIndex === 0) {
+      this.startAgainForward();
+    } else if (x === -1 && this.classIndex === this.imageData.length-1) {
+      this.startAgainBackward();
+    } else {
+      this.imageData[this.classIndex-x].class = 'actual-front';
+      this.imageData[this.classIndex-(2*x)].class = 'img-item';
+    }
+  }
+
+  startAgainForward() {
+    this.imageData[this.imageData.length-1].class = 'actual-front';
+    this.imageData[this.imageData.length-2].class = 'img-item';
+  }
+
+  startAgainBackward() {
+    this.imageData[0].class = 'actual-front';
+    this.imageData[1].class = 'img-item'; 
   }
 }
