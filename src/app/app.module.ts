42693bcab5bc7d4b8bdd907e36d1fb086a369c52
Pamela Paprasz <papraszpamela@gmail.com>
2017-06-20 10:33:04 +0200

    home route done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { ReservationComponent } from './reservation/reservation.component';
+import { SimpleHeaderComponent } from './reservation/simple-header/simple-header.component';
 
 const appRoutes: Routes =[
   { path: '', component: HomeComponent },
@@ -30,7 +31,8 @@ const appRoutes: Routes =[
     HeaderComponent,
     HomeComponent,
     ReservationComponent,
-    FooterComponent
+    FooterComponent,
+    SimpleHeaderComponent
   ],
   imports: [
     BrowserModule,
