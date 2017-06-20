9aa46276061113b63e9c6ed357e218c88f3dd18b
Daniel Korosi <daniel.korosi42@gmail.com>
2017-06-20 14:36:44 +0200

    input data from search form are collected into an array

--- src/app/reservation/check-form/check-form.component.ts
+++ src/app/reservation/check-form/check-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {IMyDpOptions} from 'mydatepicker';
 
 @Component({
   selector: 'app-check-form',
@@ -6,12 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./check-form.component.scss']
 })
 export class CheckFormComponent implements OnInit {
+   
+   private myDatePickerOptions: IMyDpOptions = {
+        dateFormat: 'dd.mm.yyyy',
+    };
 
   constructor() { }
 
   ngOnInit() {
   }
-editSearchInputs = [];
 
+  searchInputs = [];
+
+  collectItem(checkin, checkout, adults, children) {
+    this.searchInputs = [];
+    
+    if (checkin.mydate === undefined || checkin.mydate === null) {
+        checkin.mydate = {
+          formatted: ""
+        }
+      }
+      if (checkout.mydate === undefined || checkin.mydate === null) {
+        checkout.mydate = {
+        formatted: ""
+        }
+      }
+
+    this.searchInputs.push(checkin.mydate.formatted, checkout.mydate.formatted, adults, children);
+    console.log(this.searchInputs);
+  }
 
 }
