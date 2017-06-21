a6bab60a56529a98500195e58693d7821a275353
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-06-21 11:53:06 +0200

    load more button now in the hotel cards component. Load more results button works

--- src/app/reservation/hotel-cards/hotel-cards.component.ts
+++ src/app/reservation/hotel-cards/hotel-cards.component.ts
@@ -11,7 +11,8 @@ import { AppService } from '../../app.service';
 export class HotelCardsComponent implements OnInit {
 
   rooms = [];
-  
+  currentRooms = [];
+  hotelPage = 1;
 
   constructor(private roomData: AppService) { }
 
@@ -21,11 +22,24 @@ export class HotelCardsComponent implements OnInit {
         (response: Response) => {
           const cardData = response.json();
           this.rooms = cardData;
+          this.firstRoomFill();
         },
-        (error) => console.log(error)  
+        (error) => console.log(error)
       );
   }
-  
+  firstRoomFill = function() {
+    for (let i = 0; i < 10; i++) {
+      this.currentRooms.push(this.rooms[i]);
+    }
+  }
+
+  loadMoreRooms = function() {
+    let i = this.hotelPage * 10;
+    let end;
+    (this.rooms.length - this.hotelPage * 10) >= 10 ? end = this.hotelPage * 10 + 10 : end = this.rooms.length;
+    for (i; i < end; i++) {
+      this.currentRooms.push(this.rooms[i]);
+    }
+    this.hotelPage++;
+  }
 }
-
-
