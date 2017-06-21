5ba320ee77f59faf9f850feda1f40bd0ec4d325e
Pamela Paprasz <papraszpamela@gmail.com>
2017-06-21 22:33:37 +0200

    hotel-cards component is ok

--- src/app/reservation/hotel-cards/hotel-cards.component.spec.ts
+++ src/app/reservation/hotel-cards/hotel-cards.component.spec.ts
@@ -2,13 +2,23 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HotelCardsComponent } from './hotel-cards.component';
 
+import { AppService } from '../../app.service';
+
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
 describe('HotelCardsComponent', () => {
   let component: HotelCardsComponent;
   let fixture: ComponentFixture<HotelCardsComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HotelCardsComponent ]
+      declarations: [ HotelCardsComponent ],
+      imports: [
+        HttpModule,  
+        FormsModule
+      ],
+      providers: [ AppService ]
     })
     .compileComponents();
   }));
