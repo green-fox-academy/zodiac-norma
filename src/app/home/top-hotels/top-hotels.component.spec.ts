4a6ca986206ef4ca9f5adb9fc02e0c014eaad805
Pamela Paprasz <papraszpamela@gmail.com>
2017-06-21 23:56:30 +0200

    aptop hotels component is ok

---
+++ src/app/home/top-hotels/top-hotels.component.spec.ts
@@ -0,0 +1,41 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AppService } from '../../app.service';
+
+import { TopHotelsComponent } from './top-hotels.component';
+
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { Routes, RouterModule } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+
+describe('TopHotelsComponent', () => {
+  let component: TopHotelsComponent;
+  let fixture: ComponentFixture<TopHotelsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TopHotelsComponent ],
+      imports: [
+        HttpModule, 
+        FormsModule,
+        RouterTestingModule
+      ],
+      providers: [
+        AppService
+      ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TopHotelsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should be created', () => {
+    expect(component).toBeTruthy();
+  });
+});
