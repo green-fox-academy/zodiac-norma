d46477a893fb40ae745c03365e1e7b7992f6743d
Pamela Paprasz <papraszpamela@gmail.com>
2017-06-23 14:40:23 +0200

    3 tests are working on test mock server

--- src/app/reservation/hotel-cards/hotel-cards.component.spec.ts
+++ src/app/reservation/hotel-cards/hotel-cards.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, fakeAsync, tick, ComponentFixture, TestBed, inject } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
 
 import { HotelCardsComponent } from './hotel-cards.component';
 
@@ -6,15 +6,13 @@ import { AppService } from '../../app.service';
 
 import { FormsModule } from '@angular/forms';
 
-import {Observable } from 'rxjs/Rx';
-
 import { 
   HttpModule, 
   Http, 
   BaseRequestOptions,
   Response,
   ResponseOptions, 
-  XHRBackend
+  XHRBackend 
 } from '@angular/http';
 
 import { MockBackend } from '@angular/http/testing';
@@ -44,6 +42,11 @@ describe('HotelCardsComponent', () => {
     fixture.detectChanges();
   });
 
+  it('rooms length is zero', () => {   
+    let rooms = fixture.nativeElement.querySelectorAll('.rooms');
+    expect(rooms.length).toBe(0);
+  });
+
   it('should be created', () => {    
     expect(component).toBeTruthy();
   });
@@ -53,18 +56,13 @@ describe('HotelCardsComponent', () => {
     expect(appService).toEqual(component);
   });
 
-  it('button value is null', () => {
-    let button = fixture.nativeElement.querySelector('.button');
-  
-    expect(button).toBe(null);
-  });
 
   it('component opens a HTTP request', 
     inject([AppService, XHRBackend], (appService, mockBackend) => {
-
       mockBackend.connections.subscribe((connection) => {
         expect(4).toBe(4);
       });
+      component.ngOnInit()
   }));
 
   it('mockbackend gives back the right url', 
@@ -73,6 +71,44 @@ describe('HotelCardsComponent', () => {
         expect(connection.request.url).toBe("https://bookingnorma.glitch.me/rooms");
       });
       component.ngOnInit()
+  }));
+
+  it('mockbackend gives back the right url', 
+    inject([AppService, XHRBackend], (appService, mockBackend) => {
+
+    var rooms = [
+        {
+          roomtype: 'single room',
+          features: ['323-452 sq ft / 30-42 sq m ', 'Free access to spa facilities', 'Comfortable work area', 'Sweet sweeper bed', 'Separate bath and shower'],
+          price: 115 + ' €',
+          id: 1,
+          image: 'https://cdn.glitch.com/fede26cd-2622-4d50-b768-5da9b932383a%2Fpic1_mod.jpg?1497949004290'
+        },
+        {
+          roomtype: 'Classic room, non-smoking: King bed',
+          features: ['223-452 sq ft / 20-42 sq m ', 'Free access to spa facilities', 'Comfortable work area'],
+          price: 215 + ' €',
+          id: 2,
+          image: 'https://cdn.glitch.com/fede26cd-2622-4d50-b768-5da9b932383a%2Fpic2_mod.jpg?1497949136425'
+        }
+    ]    
+
+    mockBackend.connections.subscribe((connection) => {
+        connection.mockRespond(new Response(new ResponseOptions({
+          body: JSON.stringify(rooms)
+        })));
+    });
+    component.ngOnInit()
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+      let rooms = fixture.nativeElement.querySelectorAll('.rooms');
+      expect(rooms.length).toEqual(2);
+      let title = fixture.nativeElement.querySelector('.title');
+      expect(title.textContent).toContain('single');
+      let titleArray = fixture.nativeElement.querySelectorAll('.title');
+      expect(title[0].textContent).toContain('room');
+    });
   }));
 });
 
