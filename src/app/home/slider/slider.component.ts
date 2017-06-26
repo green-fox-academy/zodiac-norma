84810529d927e227a0dfd84984ec261fe90c4708
Daniel Korosi <daniel.korosi42@gmail.com>
2017-06-26 08:55:04 +0200

    comments erased

--- src/app/home/slider/slider.component.ts
+++ src/app/home/slider/slider.component.ts
@@ -34,27 +34,27 @@ export class SliderComponent implements OnInit {
     }
     
   setClassIndex(x, className) {
-    this.classIndex += x;   //increments translated class back or forth
+    this.classIndex += x;
     
-    if (this.classIndex > 0 && this.classIndex < this.imageData.length) {  //between the ends of the array
-      this.imageData[this.imageData.length-1].class = 'default';  //last image to default
-      this.imageData[0].class = 'default';  //first to be displayed
+    if (this.classIndex > 0 && this.classIndex < this.imageData.length) {
+      this.imageData[this.imageData.length-1].class = 'default';
+      this.imageData[0].class = 'default';
     } else if (this.classIndex === this.imageData.length) {
-        this.classIndex = 0;  //if reaching the end of array+1, set class at the start 
+        this.classIndex = 0;
     } else if (this.classIndex === -1){
-        this.classIndex = this.imageData.length-1; //if reaching start-1, set class at the end
+        this.classIndex = this.imageData.length-1;
     }
     this.setClassNames(x, className);
   }
 
   setClassNames(x, className) {
     this.imageData[this.classIndex].class = className;
-    if (x === 1 && this.classIndex === 0) {   //reaching end of array when clicking forward
-      this.startOver(this.imageData.length-1, this.imageData.length-2);   //last image set to be displayed during animation
-    } else if (x === -1 && this.classIndex === this.imageData.length-1) {  //reaching end of array when clicking backward
-      this.startOver(0, 1); //first image set to be displayed during animation
+    if (x === 1 && this.classIndex === 0) {
+      this.startOver(this.imageData.length-1, this.imageData.length-2);
+    } else if (x === -1 && this.classIndex === this.imageData.length-1) {
+      this.startOver(0, 1); 
     } else {
-      this.imageData[this.classIndex-x].class = 'current-front'; //
+      this.imageData[this.classIndex-x].class = 'current-front'; 
       if (this.imageData[this.classIndex-(2*x)]) {
         this.imageData[this.classIndex-(2*x)].class = 'default';
       }
