b82cfe84ac7a3d09266e1b62c439069365fa301f
Pamela Paprasz <papraszpamela@gmail.com>
2017-06-30 12:00:28 +0200

    owerview is overview

--- src/app/hotel/overview/overview.component.spec.ts
+++ src/app/hotel/overview/overview.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { OwerviewComponent } from './owerview.component';
+import { OverviewComponent } from './overview.component';
 
-describe('OwerviewComponent', () => {
-  let component: OwerviewComponent;
-  let fixture: ComponentFixture<OwerviewComponent>;
+describe('OverviewComponent', () => {
+  let component: OverviewComponent;
+  let fixture: ComponentFixture<OverviewComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ OwerviewComponent ]
+      declarations: [ OverviewComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(OwerviewComponent);
+    fixture = TestBed.createComponent(OverviewComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
