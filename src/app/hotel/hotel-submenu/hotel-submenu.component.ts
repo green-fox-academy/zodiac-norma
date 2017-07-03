69145f0bbb0de67e717a8c114813b56551fea8d5
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-07-03 15:33:04 +0200

    highlight still not work

--- src/app/hotel/hotel-submenu/hotel-submenu.component.ts
+++ src/app/hotel/hotel-submenu/hotel-submenu.component.ts
@@ -9,33 +9,29 @@ import { Router } from '@angular/router';
 
 export class HotelSubmenuComponent implements OnInit {
 
-    @ViewChildren('link') link:ElementRef;
+    @ViewChildren('linken') linken:ElementRef;
 
     constructor(private router: Router) {}
 
     ngOnInit() {
         this.router.events.subscribe((event) => {
-            this.linkHighlight(this.router.url);
+            this.linkenHighlight(this.router.url);
         });
     }
 
-    linkHighlight = function(url) {
-        for (let i = 0; i < this.link._results.length; i++) {
-            this.link._results[i].nativeElement.className = 'navigation';
+    linkenHighlight = function(url) {
+        for (let i = 0; i < this.linken._results.length; i++) {
+            this.linken._results[i].nativeElement.className = 'navigation';
         }
-        this.link._results[0].nativeElement.className = 'highlighted';
-        console.log('overview:   ' + url.slice(7, 15));
-        console.log('rooms:   ' + url.slice(7, 12));
-        console.log('map:   ' + url.slice(7, 10));
-        console.log(this.link._results[0].nativeElement.className);
         if (url.slice(7, 15) === 'overview') {
-            this.link._results[0].nativeElement.className = 'highlighted';
+            this.linken._results[0].nativeElement.className = 'highlighted';
         }
         if (url.slice(7, 12) === 'rooms') {
-            this.link._results[1].nativeElement.className = 'highlighted';
+            this.linken._results[1].nativeElement.className = 'highlighted';
         }
         if (url.slice(7, 10) === 'map') {
-            this.link._results[2].nativeElement.className = 'highlighted';
+            this.linken._results[2].nativeElement.className = 'highlighted';
         }
+        console.log(this.linken._results[2]);
     }
 }
