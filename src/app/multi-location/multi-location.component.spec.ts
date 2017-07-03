0fb00011f3188eaba8a1d494bc6c772714290ef4
Daniel Korosi <daniel.korosi42@gmail.com>
2017-07-03 21:49:38 +0200

    imports added

--- src/app/multi-location/multi-location.component.spec.ts
+++ src/app/multi-location/multi-location.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MultiLocationComponent } from './multi-location.component';
+import { HttpModule } from '@angular/http';
+import { Routes, RouterModule } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MapObjectComponent } from './map-object/map-object.component';
 
 describe('MultiLocationComponent', () => {
   let component: MultiLocationComponent;
