35300c7d15ab680bfb26f5a2cef3e5520393c2d9
Pamela Paprasz <papraszpamela@gmail.com>
2017-07-03 19:52:48 +0200

    ts is clean

--- src/app/hotel/overview/overview.component.ts
+++ src/app/hotel/overview/overview.component.ts
@@ -9,13 +9,9 @@ import {DomSanitizer} from '@angular/platform-browser';
   styleUrls: ['./overview.component.scss']
 })
 export class OverviewComponent implements OnInit {
-    name:string;
-    // video: any = {id: ''};
-    videoId;
     roomImage = {};
     roomInfoWithImage = {};
     roomInfoWithFootage = {};
-    baseUrl:string = "https://www.youtube.com/embed/";
     url;
 
     constructor(private appService: AppService, private sanitizer: DomSanitizer) { 
@@ -29,13 +25,7 @@ export class OverviewComponent implements OnInit {
                 this.roomImage = roomData;
                 this.roomInfoWithImage = roomData[0];
                 this.roomInfoWithFootage = roomData[1]
-                this.videoId = this.roomInfoWithFootage[0].footage.substring(30)
-                // this.url = this.sanitizer.bypassSecurityTrustResourceUrl(this.baseUrl + this.videoId); 
                 this.url = this.sanitizer.bypassSecurityTrustResourceUrl(this.roomInfoWithFootage[0].footage); 
-                console.log('dadad', this.roomImage);
-                console.log('image', this.roomInfoWithImage);
-                console.log('vid', this.roomInfoWithFootage[0].footage);
-                console.log('vid', this.videoId);
             },
             (error) => console.log(error)
         );
