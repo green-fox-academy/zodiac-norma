519eec313178437a25bfa6363348287061071708
Pamela Paprasz <papraszpamela@gmail.com>
2017-07-03 10:09:49 +0200

    testing iframe

--- src/app/hotel/overview/overview.component.ts
+++ src/app/hotel/overview/overview.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Response } from '@angular/http';
 import { AppService } from '../../app.service';
+// import {SafeResourceUrl, DomSanitizationService} from '@angular/platform-browser';
+import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-overview',
@@ -8,10 +10,16 @@ import { AppService } from '../../app.service';
   styleUrls: ['./overview.component.scss']
 })
 export class OverviewComponent implements OnInit {
+    name:string;
+    video: any = {id: 'wzrnuUOoFNM'};
+    baseUrl:string = 'https://www.youtube.com/embed/';
     
     roomInfo = {}
+    url;
 
-    constructor(private appService: AppService) { }
+    constructor(private appService: AppService, private sanitizer: DomSanitizer) { 
+        this.url = this.sanitizer.bypassSecurityTrustResourceUrl(this.baseUrl + this.video.id); 
+    }
     
     ngOnInit() {
         this.appService.getData('https://two-ferns.glitch.me/roomdetails')
