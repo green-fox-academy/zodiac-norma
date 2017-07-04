b4a58889a90b5b838de496ec932dc2d79b26dbad
Pamela Paprasz <papraszpamela@gmail.com>
2017-07-05 01:12:00 +0200

    play button is in center

--- src/app/hotel/overview/overview.component.ts
+++ src/app/hotel/overview/overview.component.ts
@@ -13,6 +13,7 @@ export class OverviewComponent implements OnInit {
     roomInfoWithImage = {};
     roomInfoWithFootage = {};
     url;
+    urlToClick;
     thumbnail;
     id;
 
@@ -27,14 +28,19 @@ export class OverviewComponent implements OnInit {
                 this.roomImage = roomData;
                 this.roomInfoWithImage = roomData[0];
                 this.roomInfoWithFootage = roomData[1];
+                this.urlToClick = this.roomInfoWithFootage[0].footage;
                 this.url = this.sanitizer.bypassSecurityTrustResourceUrl(this.roomInfoWithFootage[0].footage); 
-                this.id = this.roomInfoWithFootage[0].footage.substr(30);
+                this.id = this.roomInfoWithFootage[0].footage.slice(30, 41);
                 this.thumbnail = 'https://img.youtube.com/vi/'+this.id+'/0.jpg'  
                 console.log('thumb', this.thumbnail);
                           
             },
             (error) => console.log(error)
         );
+    }
+
+    autoplay() {
+        this.urlToClick + '?autoplay=1'
     }
 
 
